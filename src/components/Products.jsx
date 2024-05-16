5f031662d7afe93b4f8c70b4054aa734fac6a2da
muddabirm <33209109+muddabirm@users.noreply.github.com>
2024-05-15 19:31:43 -0500

    made some frontend changes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,11 +4,23 @@ import product from '../images/product.jpg'
 const Products = () => {
   return (
     <>
-        <div className='w-72 h-80 border-2'>
+    <div className='flex justify-center pt-10'>
+        <div className='w-72 h-80 border-2 p-7 mr-6 rounded-md '>
         <img src={product} alt="" />
-        <h2>1500$</h2>
-        <button>Add to cart</button>
+        <h2 className='font-bold'>1500$</h2>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
         </div>
+        <div className='w-72 h-80 border-2 p-7 mr-6 rounded-md'>
+        <img src={product} alt="" />
+        <h2 className='font-bold'>1500$</h2>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
+        </div>
+        <div className='w-72 h-80 border-2 p-7 rounded-md'>
+        <img src={product} alt="" />
+        <h2 className='font-bold'>1500$</h2>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
+        </div>
+      </div>
     </>
   )
 }
