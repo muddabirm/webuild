375a68c35e7a92e0ade31e9530417147bb93c127
muddabirm <33209109+muddabirm@users.noreply.github.com>
2024-05-20 03:54:46 -0500

    changed some frontend images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,33 +4,32 @@ import React from 'react'
 const navbar = () => {
   return (
     <>
-    <div className='flex justify-between p-5'>
-      <h2 className='flex'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
-</svg>
-WeBuild</h2>
-      
-      <ul className='flex'>
-        <li className='pr-5'><a href='#'>Gaming PCs</a></li>
-        <li className='pr-5'><a href='#'>Gaming Laptops</a></li>
-        <li><a href='#'>Sales</a></li>
-      </ul>
-      <div className='flex'>
-      <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-</svg></button>
+      <div className='flex justify-between p-5'>
+        <h2 className='flex'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
+        </svg>WeBuild</h2>
 
-      <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5
+        <ul className='flex'>
+          <li className='pr-5'><a href='#'>Gaming PC's</a></li>
+          <li className='pr-5'><a href='#'>Gaming Laptops</a></li>
+          <li><a href='#'>Sales</a></li>
+        </ul>
+        <div className='flex'>
+          <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg></button>
+
+          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5
       ">
-  <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z" clipRule="evenodd" />
-</svg></button>
-  <button>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-</svg>
-  </button>
+            <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z" clipRule="evenodd" />
+          </svg></button>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
     </>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,24 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import product from '../images/product.jpg'
+import graphicsCard from '../images/graphicsCard.jpg'
 const Products = () => {
   return (
     <>
-    <div className='flex justify-center pt-10'>
+      <div className='flex justify-center'>
+        <img className='w-full h-50 object-cover' src={graphicsCard}></img>
+      </div>
+      <div className='flex justify-center pt-10'>
         <div className='w-72 h-80 border-2 p-7 mr-6 rounded-md '>
-        <img src={product} alt="" />
-        <h2 className='font-bold'>1500$</h2>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
+          <img src={product} alt="" />
+          <h2 className='font-bold'>1500$</h2>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
         </div>
         <div className='w-72 h-80 border-2 p-7 mr-6 rounded-md'>
-        <img src={product} alt="" />
-        <h2 className='font-bold'>1500$</h2>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
+          <img src={product} alt="" />
+          <h2 className='font-bold'>1500$</h2>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
         </div>
         <div className='w-72 h-80 border-2 p-7 rounded-md'>
-        <img src={product} alt="" />
-        <h2 className='font-bold'>1500$</h2>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
+          <img src={product} alt="" />
+          <h2 className='font-bold'>1500$</h2>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add to cart</button>
         </div>
       </div>
     </>
